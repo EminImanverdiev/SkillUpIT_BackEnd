8a70274b94a5c9d290dbb24d25e55c498d8cd867
elmin007 <imanverdiyevemin414@gmail.com>
2025-01-15 00:07:17 +0400

    Added contactform and contactinfo entities to project

---
+++ Core/SkillUpIT.Domain/Entities/ContactForm.cs
@@ -0,0 +1,17 @@
+﻿using SkillUpIT.Domain.Entities.Comman;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillUpIT.Domain.Entities
+{
+    public class ContactForm:BaseEntity<Guid>
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ Core/SkillUpIT.Domain/Entities/ContactInfo.cs
@@ -0,0 +1,12 @@
+﻿using SkillUpIT.Domain.Entities.Comman;
+
+namespace SkillUpIT.Domain.Entities
+{
+    public class ContactInfo : BaseEntity<Guid>
+    {
+        public string Icon { get; set; }        
+        public string Title { get; set; }       
+        public string Description { get; set; } 
+        public string Value { get; set; }
+    }
+}
